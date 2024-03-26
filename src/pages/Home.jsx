f2fd1ac74869214aaaef60e648f14f677bf7612f
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2024-03-26 22:18:36 +0600

    added this navbar section done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+<h3>Home page</h3>
+        </div>
+    );
+};
+
+export default Home;
